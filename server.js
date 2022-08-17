f669d37464cbb2a48bc9c5116400e3e1a70505b8
Venkat Karanam <karanamvasu871@gmail.com>
2022-08-17 23:19:52 +0530

    deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //Install express server
 const express = require('express');
-const path = require('path');
+res.sendFile(path.join(__dirname, './public', 'Apr29Home.html'));
 
 const app = express();
 
