2ec4c3aa8f2350108241cdb677c0b7604d20d642
Venkat Karanam <karanamvasu871@gmail.com>
2022-08-17 20:27:05 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularForm';
+  title = 'helloworld';
+  submitted= false;
+  result={
+    firstname:'',
+    email:'',
+    number:'',
+    password:'',
+    companyname:'',
+    dob:'',
+    file:''
+  };
+  size=0;
+  url='';
+  warn='';
+  ngOnInit() {
+    
+ }
+
+ onselectfile(event  :any){
+    if(event.target.files){
+      this.size=event.target.files[0].size;
+      console.log(this.size);
+      var reader= new FileReader();
+      reader.readAsDataURL(event.target.files[0]);
+      reader.onload=(_event) =>{
+        if(this.size>2048000){
+          this.warn='File size larger than 2MB';
+        }
+        else if(this.size<2048000){
+            this.warn='';
+            this.url=reader.result as string;
+        }
+       
+      }
+    }
+
+ }
+
+  onSubmit(value :any){
+    if(this.size<2048000){
+       this.submitted=true; 
+    this.result=value;
+    console.log(this.result)
+    }
+   
+  }
+
 }
